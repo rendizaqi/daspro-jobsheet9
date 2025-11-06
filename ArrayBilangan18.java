ec685abc49949a79a6b9bfaf1ac734229e467f8f
“rendiizaqi” <rendirealme34@gmail.com>
2025-11-06 14:33:25 +0700

    Modifikasi Percobaan 1

--- ArrayBilangan18.java
+++ ArrayBilangan18.java
@@ -1,11 +1,6 @@
 public class ArrayBilangan18 {
     public static void main(String[] args) {
-        int[] bil = new int[4];
-
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        double[] bil = {5.0, 12867, 7.5, 2000000};
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
