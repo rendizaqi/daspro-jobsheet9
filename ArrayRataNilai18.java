0b59840ec8e924bab92803ea1543c8e96e8f95d8
“rendiizaqi” <rendirealme34@gmail.com>
2025-11-06 18:03:01 +0700

    Modifikasi 1 Percobaan 3

--- ArrayRataNilai18.java
+++ ArrayRataNilai18.java
@@ -7,19 +7,27 @@ public class ArrayRataNilai18 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+        int jumlahLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
 
+        // perulangan untuk menghitung jumlah nilai total dan menghitung mahasiswa lulus
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            total += nilaiMhs[i];  // Menambahkan nilai mahasiswa ke total
+            if (nilaiMhs[i] > 70) {  // Mengecek apakah mahasiswa lulus
+                jumlahLulus++;  // Menambah jumlah mahasiswa yang lulus
+            }
         }
 
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        rata2 = total / nilaiMhs.length;  // Total dibagi jumlah elemen array
+        System.out.println("Rata-rata nilai = " + rata2);  // Menampilkan rata-rata
 
-        sc.close();
+        // Menampilkan jumlah mahasiswa yang lulus
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahLulus);
+
+        sc.close();  // Menutup scanner
     }
 }
