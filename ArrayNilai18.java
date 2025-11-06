89a6eaade04d4148f01bc38d8d02db8bd07b2d5d
“rendiizaqi” <rendirealme34@gmail.com>
2025-11-06 17:41:29 +0700

    Modifikasi Percobaan 2 (Soal 4)

--- ArrayNilai18.java
+++ ArrayNilai18.java
@@ -6,15 +6,19 @@ public class ArrayNilai18 {
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-    for (int i = 0; i < nilaiAkhir.length; i++) {
-    if (nilaiAkhir[i] > 70) {
-        System.out.println("Mahasiswa ke-" + i + " lulus!");
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {  // Jika nilai lebih dari 70
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } else {  // Jika nilai kurang dari atau sama dengan 70
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
+            }
         }
-    }
+
+        sc.close();
     }
 }
